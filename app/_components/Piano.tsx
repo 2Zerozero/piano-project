4ce308bdc0c590e1c774799ce243847fd7a73677
2Zerozero <hawgae@gmail.com>
2024-07-10 17:22:09 +0900

    feat: 키보드에 맞게 피아노 음 구현 완료

--- app/_components/Piano.tsx
+++ app/_components/Piano.tsx
@@ -1,6 +1,50 @@
-import React from 'react';
+'use client';
+import { useEffect } from 'react';
 
 const Piano = () => {
+  const keyToPianoNoteMap: { [key: string]: string } = {
+    a: '/sounds/a.wav',
+    s: '/sounds/s.wav',
+    d: '/sounds/d.wav',
+    f: '/sounds/f.wav',
+    g: '/sounds/g.wav',
+    h: '/sounds/h.wav',
+    j: '/sounds/j.wav',
+    k: '/sounds/k.wav',
+    l: '/sounds/l.wav',
+    ';': '/sounds/;.wav',
+    w: '/sounds/w.wav',
+    e: '/sounds/e.wav',
+    t: '/sounds/t.wav',
+    y: '/sounds/y.wav',
+    u: '/sounds/u.wav',
+    o: '/sounds/o.wav',
+    p: '/sounds/p.wav',
+  };
+
+  const playNote = (noteFile: string) => {
+    const audio = new Audio(noteFile);
+
+    audio.play();
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const nodeFile = keyToPianoNoteMap[event.key];
+      if (nodeFile) {
+        playNote(nodeFile);
+
+        console.log(nodeFile);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return <div>Piano</div>;
 };
 
